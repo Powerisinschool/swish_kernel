89d30743727b12ac91b82fd9fe0a780b495040c7
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2026-06-15 14:47:34 +0100

    Refactor x86_64 architecture support: implement CPU, IDT, GDT, and interrupt handling

---
+++ include/arch/cpu.h
@@ -0,0 +1,8 @@
+//
+// Created by Tolulope Olagunju on 15/06/2026.
+//
+
+#ifndef BAREMETALKERNEL_CPU_H
+#define BAREMETALKERNEL_CPU_H
+
+#endif //BAREMETALKERNEL_CPU_H
---
+++ src/arch/x86_64/gdt.cpp
@@ -0,0 +1,1 @@
+#include <../../include/arch/x86_64/gdt.h>
