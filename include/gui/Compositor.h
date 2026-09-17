5da81ef14434bd0bfc4556a4efa786c94196a0cd
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2026-09-18 00:39:39 +0100

    Make `delete_window` and `cleanup_closed_windows` method declarations private in Compositor class for improved structure.

--- include/gui/Compositor.h
+++ include/gui/Compositor.h
@@ -12,8 +12,6 @@ public:
     explicit Compositor(Surface *surface);
     void add_window(Window* window);
     void focus_window(const Window* window);
-    void delete_window(CompositorWindow* win);
-    void cleanup_closed_windows();
     void inject_key(char c) const;
     void inject_mouse_button(uint64_t x, uint64_t y, uint8_t button, bool is_down);
     void inject_mouse_move(const uint64_t x, const uint64_t y) const;
@@ -22,4 +20,7 @@ private:
     Surface *rootSurface;
     CompositorWindow *head;
     CompositorWindow *tail;
+
+    void delete_window(CompositorWindow* win);
+    void cleanup_closed_windows();
 };
